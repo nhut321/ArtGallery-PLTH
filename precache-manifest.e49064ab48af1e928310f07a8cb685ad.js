d2b53cb70800df982a671bab0c1c5538f5cbdee6
nhut321 <znhut12z@gmail.com>
2025-02-04 00:40:46 +0700

    new commit

---
+++ precache-manifest.e49064ab48af1e928310f07a8cb685ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a45a854dadc520fcdbd0f1f9aeeb1a0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "695fbe6886d132c7a040",
+    "url": "./static/css/main.b5843022.chunk.css"
+  },
+  {
+    "revision": "c157981ca541a44f6d31",
+    "url": "./static/js/2.1c80904d.chunk.js"
+  },
+  {
+    "revision": "ba06d90c0f68ea94056646cd50d2212c",
+    "url": "./static/js/2.1c80904d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695fbe6886d132c7a040",
+    "url": "./static/js/main.95827eeb.chunk.js"
+  },
+  {
+    "revision": "3411b0c17102f3bcf807",
+    "url": "./static/js/runtime-main.4f996271.js"
+  },
+  {
+    "revision": "0fa74170ede8f9ec7330d04c6ad695ee",
+    "url": "./static/media/logo.0fa74170.png"
+  },
+  {
+    "revision": "b0f534fc7d9d31f2165c7f670c538c46",
+    "url": "./static/media/music.b0f534fc.mp3"
+  }
+]);
