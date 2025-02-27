48a5dbd3b38373c7b2780d81e5a203a146cf809c
nhut321 <znhut12z@gmail.com>
2025-02-27 16:26:14 +0700

    update chat

---
+++ precache-manifest.65f52ef8980d2c3c0b4f0b542c55fade.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0ae3be13d1e34fecc04d030310ce90",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20d2f5b758df3081db9a",
+    "url": "./static/css/main.dbc8bd5a.chunk.css"
+  },
+  {
+    "revision": "cb62c994dad599fd6414",
+    "url": "./static/js/2.96db3168.chunk.js"
+  },
+  {
+    "revision": "ba06d90c0f68ea94056646cd50d2212c",
+    "url": "./static/js/2.96db3168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20d2f5b758df3081db9a",
+    "url": "./static/js/main.e492dfcc.chunk.js"
+  },
+  {
+    "revision": "3411b0c17102f3bcf807",
+    "url": "./static/js/runtime-main.4f996271.js"
+  },
+  {
+    "revision": "0fa74170ede8f9ec7330d04c6ad695ee",
+    "url": "./static/media/logo.0fa74170.png"
+  },
+  {
+    "revision": "b8f06cd1a695f26f2ea423aa01d99ec8",
+    "url": "./static/media/music.b8f06cd1.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4107f048e5a4a20fb223c14f0211bd5b.js"
+  "./precache-manifest.65f52ef8980d2c3c0b4f0b542c55fade.js"
 );
 
 self.addEventListener('message', (event) => {
